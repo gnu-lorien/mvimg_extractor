70e2b60c4d981338da4f731253079ff06269bd90
Andrew Sayman <andrew.sayman@gmail.com>
2021-10-25 21:19:36 -0400

    Scan the xmp data manually
    The format was updated and nothing seems to be able to read these particular xmp style sections. https://gitlab.gnome.org/GNOME/shotwell/-/issues/233

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import glob
 import argparse
 import io
 from pyexiv2 import Image
+import pyavm
+from PIL import Image
+import re
+
+def offset_from_microvideo(md):
+    try:
+        return int(md["Xmp.GCamera.MicroVideoOffset"])
+    except KeyError:
+        print(f"Skipping {filename}")
+    return None
+
+def offset_from_directory(md):
+    pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -12,10 +25,14 @@ if __name__ == "__main__":
 
     for filename in glob.iglob(args.pathname, recursive=args.recursive):
         print(filename)
-        img = Image(filename)
-        md = img.read_xmp()
-        img.close()
-        reverse_offset = int(md["Xmp.GCamera.MicroVideoOffset"])
+        with open(filename, 'rb') as f:
+            contents = f.read()
+        start_motion_photo = [m.start() for m in re.finditer(b"Item:Semantic=\"MotionPhoto\"", contents)]
+        if 0 == len(start_motion_photo):
+            continue
+        motion_length = [m for m in re.finditer(b"Item:Length=\"(\d+)\"", contents[start_motion_photo[0]:])]
+        reverse_offset = motion_length[0].group(1)
+        reverse_offset = int(reverse_offset)
 
         with open(filename, "rb") as f:
             f.seek(reverse_offset * -1, 2)
